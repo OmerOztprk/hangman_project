fbf8fe2f534a56a39d83f284228d57b893f3f278
omeroztprk <omer.56760@gmail.com>
2025-02-07 13:14:02 +0300

    feat: add lives and game over condition with hangman stages

--- main.py
+++ main.py
@@ -1,4 +1,60 @@
 import random
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["aardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
@@ -11,6 +67,7 @@ for position in range(word_length):
 print(placeholder)
 
 game_over = False
+lives = 6
 correct_letters = ["_" for _ in range(len(chosen_word))]
 
 while not game_over:
@@ -19,8 +76,16 @@ while not game_over:
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             correct_letters[i] = guess
+
+    if guess not in chosen_word:
+        lives -= 1
+
     print("".join(correct_letters))
+    print(stages[lives])
 
     if "_" not in correct_letters:
         game_over = True
         print("You win!")
+    elif lives == 0:
+        game_over = True
+        print("You lose!")
